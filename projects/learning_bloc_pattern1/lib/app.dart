6934e562562b3e0de7ce6020cb2ee74cb7141014
Gyusik-Choi <57178206+Gyusik-Choi@users.noreply.github.com>
2022-04-02 15:09:15 +0900

    Delete projects/learning_bloc_pattern1 directory

--- projects/learning_bloc_pattern1/lib/app.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import './view/counter_page.dart';
-
-class CounterApp extends MaterialApp {
-  const CounterApp({Key? key}) : 
-    super(key: key, home: const CounterPage());
-}
