c388c28306453c388a8d87772b9e4ff090e8f5b1
kuidoli <kuidoli@hanmail.net>
2022-04-15 23:42:57 +0900

    create and apply dotenv

--- projects/box_office_1/lib/main.dart
+++ projects/box_office_1/lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:box_office_1/repository/movie_api.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
+  await dotenv.load(fileName: '.env');
+
   MovieAPI movieApi = MovieAPI();
   await movieApi.getMovies();
-  runApp(MyApp());
+
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
--- projects/box_office_1/lib/repository/movie_api.dart
+++ projects/box_office_1/lib/repository/movie_api.dart
@@ -1,11 +1,13 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class MovieAPI {
-  String key = 'k_8e9i8qvy';
+  String url = 'https://imdb-api.com/en/API/BoxOffice/';
+  String key = dotenv.get('KEY');
 
   Future<void> getMovies() async {
-    http.Response response = await http.get(Uri.parse('https://imdb-api.com/en/API/BoxOffice/k_8e9i8qvy'));
+    http.Response response = await http.get(Uri.parse(url + key));
     Map<String, dynamic> responseBody = jsonDecode(response.body);
     print(responseBody);
   }
