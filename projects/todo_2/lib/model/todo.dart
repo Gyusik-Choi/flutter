cc0d5322d05f07044dcd958c26016fb520430b53
kuidoli <kuidoli@hanmail.net>
2022-05-01 23:20:40 +0900

    create files and todo class

---
+++ projects/todo_2/lib/model/todo.dart
@@ -0,0 +1,20 @@
+class Todo {
+  int id;
+  String description;
+  bool isDone;
+
+  Todo({required this.id, required this.description, this.isDone = false});
+
+  Todo.fromMap(Map<String, dynamic> itemFromDB)
+    : id = itemFromDB['id'],
+      description = itemFromDB['description'],
+      isDone = itemFromDB['isDone'];
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'description': description,
+      'is_done': isDone == false ? 0 : 1
+    };
+  }
+}
