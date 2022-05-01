f7c9d78f4605d26cf0a001d928fe4a17278ddb78
kuidoli <kuidoli@hanmail.net>
2022-05-01 23:10:02 +0900

    create structure of this project

--- projects/todo_2/lib/main.dart
+++ projects/todo_2/lib/main.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:todo_2/ui/home.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return MaterialApp(
+      home: Home(),
     );
   }
 }
